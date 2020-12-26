7c58c5cc3354dfdc974dfd132c12311af4094a0c
DeepaNair106 <72653999+DeepaNair106@users.noreply.github.com>
2020-12-26 14:31:24 +0530

    Add files via upload

---
+++ Lets Chat.js
@@ -0,0 +1,7 @@
+function addUser() {
+
+    userName = document.getElementById("user_name").value;
+    localStorage.setItem("userName",userName);
+    window.location = "Lets Chat_room.html";
+    
+}
---
+++ Lets Chat_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA5nIX-xXc-Q2j9NuJ_Q4L8YqL3ibgyDIo",
+    authDomain: "lets-chat-b324e.firebaseapp.com",
+    projectId: "lets-chat-b324e",
+    storageBucket: "lets-chat-b324e.appspot.com",
+    messagingSenderId: "389666343189",
+    appId: "1:389666343189:web:b21843934b1726750e811c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
